32f1dd20554db18838f16644221e36997311a143
namtae <kimnamtae1230@gmail.com>
2024-08-10 15:47:53 +0900

    add occlumesh dataloader

--- dataLoader/__init__.py
+++ dataLoader/__init__.py
@@ -3,10 +3,13 @@ from dataLoader.google_scanned_objects import GoogleObjsDataset
 from dataLoader.instant3d import Instant3DObjsDataset
 from dataLoader.mipnerf import MipNeRF360Dataset
 from dataLoader.mvgen import MVGenDataset
+from dataLoader.occlumesh import Occlumesh
+
 
 dataset_dict = {'gobjeverse': gobjverse, 
                 'GSO': GoogleObjsDataset,
                 'instant3d': Instant3DObjsDataset,
                 'mipnerf360': MipNeRF360Dataset,
                 'mvgen': MVGenDataset,
+                'Occlumesh' : Occlumesh 
                 }
